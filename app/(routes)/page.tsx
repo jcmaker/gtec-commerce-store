5187e2b6910dff724f859e0b1b32ba8b188f3a29
jcmaker0627 <jcmaker0627@gmail.com>
2023-08-02 01:52:29 +0900

    0802 final

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("e7d268d2-a711-4d2d-ae24-b6a58f5ed836");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="전체" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
---
+++ components/ui/no-results.tsx
@@ -0,0 +1,9 @@
+const NoResults = () => {
+  return (
+    <div className="flex items-center justify-center h-full w-full text-neutral-500">
+      아이템 비어있음.
+    </div>
+  );
+};
+
+export default NoResults;
